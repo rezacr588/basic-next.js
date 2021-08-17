fb8daa6eb30b8e331c92cefc6a6bd2dea7c20ce0
Reza Zeraat <rezacr588@gmail.com>
2021-08-17 22:37:09 +0430

    access to router params

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,4 +1,11 @@
+import { useRouter } from 'next/router'
+
 function PortfolioProjectIdPage() {
+  const router = useRouter()
+
+  console.log(router.query);
+  console.log(router.pathname);
+
   return <div>
     <h1>
       Portfolio Project Id Page

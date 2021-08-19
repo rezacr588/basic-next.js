317ca0cbf117f0631b4c8daa8a6a3e3de0da6666
Reza Zeraat <rezacr588@gmail.com>
2021-08-19 14:11:37 +0430

    dynamic navigation

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,30 @@
+import Link from "next/link"
+import { useRef } from "react"
+
 function ClientsPage() {
-  return <div>
-    Clients Page
-  </div>
+  const clients = useRef([
+    { id: "reza", name: "reza" },
+    { id: "ali", name: "ali" },
+  ])
+  return (
+    <div>
+      <h1>
+          Clients Page
+      </h1>
+      <ul>
+        {
+          clients.current.map(
+            client =>
+              <li key={client.id}>
+                <Link  href={`/clients/${client.id}`}>
+                  {client.name}
+                </Link>
+              </li>
+            )
+          }
+      </ul>
+    </div>
+  )
 }
 
 export default ClientsPage

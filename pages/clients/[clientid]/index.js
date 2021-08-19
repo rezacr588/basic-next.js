4373cde11001d4973cc065d6107339fa1977739e
Reza Zeraat <rezacr588@gmail.com>
2021-08-19 15:21:18 +0430

    navigate programmitally

--- pages/clients/[clientid]/index.js
+++ pages/clients/[clientid]/index.js
@@ -1,7 +1,26 @@
+import { useRouter } from "next/router"
 function ClientIdPage() {
-  return <div>
-    Client Id Page
-  </div>
+  const router = useRouter()
+
+  const handle = () => 
+    router.push(
+      {
+        pathname: "/clients/[clientid]/[projectid]",
+        query: {
+          clientid: "213",
+          projectid: "2313"
+        }
+      }
+    )
+  
+  return (
+    <div>
+      <h1>
+        Client Id Page
+      </h1>
+      <button onClick={handle}>project</button>
+    </div>
+  )
 }
 
 export default ClientIdPage
